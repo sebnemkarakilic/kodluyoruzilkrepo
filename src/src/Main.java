dec3a28616320a970c0502a4eb81f00c7105ac56
sebnemkarakilic <sebnemkarakilic@gmail.com>
2024-11-12 15:56:53 +0100

    ATM

--- src/src/Main.java
+++ src/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        ATM(input);
         makeDiamondFromStars(input);
         makeTriangleFromStars(input);
         findHarmonicNumbers(input);
@@ -30,6 +31,66 @@ public class Main {
         calculateVAT(input);
         calculateGPA(input);
 
+    }
+
+    //ATM
+    public static void ATM(Scanner input) {
+        String userName, password;
+        int right = 3;
+        int balance = 1500;
+        int select = 0;
+        while (right > 0) {
+            System.out.print("Kullanıcı Adınız :");
+            userName = input.nextLine();
+            System.out.print("Parolanız : ");
+            password = input.nextLine();
+
+            if (userName.equals("patika") && password.equals("dev123")) {
+                System.out.println("Merhaba, Kodluyoruz Bankasına Hoşgeldiniz!");
+
+                while (select != 4) {
+                    System.out.println("""
+                            1-Para yatırma
+                            2-Para Çekme
+                            3-Bakiye Sorgula
+                            4-Çıkış Yap""");
+                    System.out.print("Lütfen yapmak istediğiniz işlemi seçiniz : ");
+                    select = input.nextInt();
+                    switch (select) {
+                        case 1:
+                            System.out.print("Para miktarı : ");
+                            int depositedMoney = input.nextInt();
+                            balance += depositedMoney;
+                            break;
+                        case 2:
+                            System.out.print("Para miktarı : ");
+                            int withdrawMoney = input.nextInt();
+                            if (withdrawMoney > balance) {
+                                System.out.println("Bakiye yetersiz.");
+                            } else {
+                                balance -= withdrawMoney;
+                            }
+                            break;
+                        case 3:
+                            System.out.println("Bakiyeniz : " + balance);
+                            break;
+                        case 4:
+                            System.out.println("Tekrar görüşmek üzere.");
+                            break;
+                    }
+                }
+                break;
+            } else {
+                right--;
+                System.out.println("Hatalı kullanıcı adı veya şifre. Tekrar deneyiniz.");
+                if (right == 0) {
+                    System.out.println("Hesabınız bloke olmuştur lütfen banka ile iletişime geçiniz.");
+                } else {
+                    System.out.println("Kalan Hakkınız : " + right);
+
+                }
+            }
+        }
     }
 
     //Make Triangle from stars
