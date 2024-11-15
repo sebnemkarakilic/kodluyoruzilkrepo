fe31dc37a4a50c3bcaf37dc0622d065a5568e957
sebnemkarakilic <sebnemkarakilic@gmail.com>
2024-11-15 11:23:01 +0100

    Öğrenci Bilgi Sistemi

--- src/src/Main.java
+++ src/src/Main.java
@@ -4,6 +4,38 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        //OOP//
+
+        Course mat = new Course("Matematik", "MAT101", "MAT");
+        Course fizik = new Course("Fizik", "FZK101", "FZK");
+        Course kimya = new Course("Kimya", "KMY101", "KMY");
+
+        Teacher t1 = new Teacher("Mahmut Hoca", "90550000000", "MAT");
+        Teacher t2 = new Teacher("Fatma Ayşe", "90550000001", "FZK");
+        Teacher t3 = new Teacher("Ali Veli", "90550000002", "KMY");
+
+        mat.addTeacher(t1);
+        fizik.addTeacher(t2);
+        kimya.addTeacher(t3);
+
+        Student s1 = new Student("İnek Şaban", 4, "140144015", mat, fizik, kimya);
+        s1.addBulkExamNote(50,20,40);
+        s1.addVerbalExamNote(80,90,70);
+        s1.isPass();
+
+        Student s2 = new Student("Güdük Necmi", 4, "2211133", mat, fizik, kimya);
+        s2.addBulkExamNote(100,50,40);
+        s2.addVerbalExamNote(70,90,80);
+        s2.isPass();
+
+        Student s3 = new Student("Hayta İsmail", 4, "221121312", mat, fizik, kimya);
+        s3.addBulkExamNote(50,20,40);
+        s3.addVerbalExamNote(90,90,80);
+        s3.isPass();
+        System.out.println("=============================");
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////
+
         modifyNumberRecursively(20,20, false);
 
         System.out.println();
---
+++ src/src/Student.java
@@ -0,0 +1,100 @@
+public class Student {
+    String name,stuNo;
+    int classes;
+    Course mat;
+    Course fizik;
+    Course kimya;
+    double avarage;
+    boolean isPass;
+    int matVerbal;
+    int fizikVerbal;
+    int kimyaVerbal;
+    double matAvg;
+    double kimAvg;
+    double fizAvg;
+
+
+    Student(String name, int classes, String stuNo, Course mat,Course fizik,Course kimya) {
+        this.name = name;
+        this.classes = classes;
+        this.stuNo = stuNo;
+        this.mat = mat;
+        this.fizik = fizik;
+        this.kimya = kimya;
+        calcAvarage();
+        this.isPass = false;
+    }
+
+
+    public void addBulkExamNote(int mat, int fizik, int kimya) {
+
+        if (mat >= 0 && mat <= 100) {
+            this.mat.note = mat;
+        }
+
+        if (fizik >= 0 && fizik <= 100) {
+            this.fizik.note = fizik;
+        }
+
+        if (kimya >= 0 && kimya <= 100) {
+            this.kimya.note = kimya;
+        }
+
+    }
+
+    public void addVerbalExamNote(int matVerbal, int fizikVerbal, int kimyaVerbal) {
+
+        if (matVerbal >= 0 && matVerbal <= 100) {
+            this.matVerbal = matVerbal;
+        }
+
+        if (fizikVerbal >= 0 && fizikVerbal <= 100) {
+            this.fizikVerbal = fizikVerbal;
+        }
+
+        if (kimyaVerbal >= 0 && kimyaVerbal <= 100) {
+            this.kimyaVerbal = kimyaVerbal;
+        }
+    }
+
+    public void isPass() {
+        if (this.mat.note == 0 || this.fizik.note == 0 || this.kimya.note == 0 || this.matVerbal == 0 || this.fizikVerbal == 0 || this.kimyaVerbal == 0) {
+            System.out.println("Notlar tam olarak girilmemiş");
+        } else {
+            this.isPass = isCheckPass();
+            printNote();
+            System.out.println("Ortalama : " + (int)this.avarage);
+            if (this.isPass) {
+                System.out.println("Sınıfı Geçti. ");
+            } else {
+                System.out.println("Sınıfta Kaldı.");
+            }
+        }
+    }
+
+    public void calcAvarage() {
+        avgNotes();
+        this.avarage = (this.matAvg + this.fizAvg + this.kimAvg) / 3;
+    }
+
+
+    public boolean isCheckPass() {
+        calcAvarage();
+        return this.avarage > 55;
+    }
+
+    public void avgNotes() {
+        this.matAvg = (this.mat.note * 0.8) + (this.matVerbal * 0.2);
+        this.fizAvg = (this.fizik.note * 0.8) + (this.fizikVerbal * 0.2);
+        this.kimAvg = (this.kimya.note * 0.8) + (this.kimyaVerbal * 0.2);
+    }
+
+    public void printNote(){
+        System.out.println("=========================");
+        System.out.println("Öğrenci : " + this.name);
+        avgNotes();
+        System.out.println("Matematik Yazili Notu : " + this.mat.note + " / Matematik Sözlü Notu : " + this.matVerbal + " / Matematik Dersi Ortalama Notu : " + this.matAvg);
+        System.out.println("Fizik Yazili Notu : " + this.fizik.note + " / Fizik Sözlü Notu : " + this.fizikVerbal + " / Fizik Dersi Ortalama Notu : " + this.fizAvg);
+        System.out.println("Kimya Yazili Notu : " + this.kimya.note + " / Kimya Sözlü Notu : " + this.kimyaVerbal + " / Kimya Dersi Ortalama Notu : " + this.kimAvg);
+    }
+}
