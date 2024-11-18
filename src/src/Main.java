9f59927ffd71ed3eac0cad74781ba8bc3c176aee
sebnemkarakilic <sebnemkarakilic@gmail.com>
2024-11-18 14:08:20 +0100

    Çok boyutlu diziler kullanılarak "yıldızlar" ile ekrana "B" harfi yazan program.

--- src/src/Main.java
+++ src/src/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        printLettesWithMultidimensionalArrays();
         findMinAndMaxArray(input);
         findAverageOfElements();
 
@@ -121,6 +122,32 @@ public class Main {
         calculateVAT(input);
         calculateGPA(input);
 
+    }
+
+    public static void printLettesWithMultidimensionalArrays() {
+        String[][] letter = new String[7][5];
+
+        for (int i = 0; i < letter.length; i++) {
+            for (int j = 0; j < letter[i].length; j++) {
+                if (i == 0 || i == 3 || i == 6) {
+                    letter[i][j] = " * ";
+                    letter[0][4] = "   ";
+                    letter[3][4] = "   ";
+                    letter[6][4] = "   ";
+                } else if (j == 0 || j == 4) {
+                    letter[i][j] = " * ";
+                } else {
+                    letter[i][j] = "   ";
+                }
+            }
+        }
+
+        for (String[] row : letter){
+            for (String col : row){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
     }
 
     public static void findMinAndMaxArray(Scanner input){
