1d400e652713d52435e03d6d7530bb8579075c4c
sebnemkarakilic <sebnemkarakilic@gmail.com>
2024-11-20 14:50:25 +0100

    isPalindrome

--- src/src/Main.java
+++ src/src/Main.java
@@ -6,6 +6,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        System.out.println(isPalindrome("abba"));
+        System.out.println(isPalindrome2("kayak"));
         guessNumberGame();
         findTranspose();
         findFrequency();
@@ -129,6 +131,29 @@ public class Main {
         calculateVAT(input);
         calculateGPA(input);
 
+    }
+
+    public static boolean isPalindrome(String str) {
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public static boolean isPalindrome2(String str) {
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+
+        if (str.equals(reverse))
+            return true;
+        else
+            return false;
     }
 
     public static void guessNumberGame() {
